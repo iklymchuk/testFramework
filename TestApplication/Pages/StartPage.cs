2b2bd961a0c46ab3fba8963156fbe414509a7bab
iklymchuk <klymhuk.ivan@gmail.com>
2017-01-31 22:43:18 +0200

    [IK] add the TestApplication project

---
+++ TestApplication/Pages/StartPage.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestFramework.Base;
+
+namespace TestApplication.Pages
+{
+    class StartPage : BasePage
+    {
+        public StartPage(IWebDriver driver) : base(driver)
+        { 
+
+        }
+
+        [FindsBy(How = How.LinkText, Using = "Log in")]
+        public IWebElement linkLogin { get; set; }
+
+        [FindsBy(How = How.Id, Using = "Username")]
+        public IWebElement textUsername { get; set; }
+
+        [FindsBy(How = How.Id, Using = "Password")]
+        public IWebElement textPassword { get; set; }
+
+        [FindsBy(How = How.Id, Using = "Submit")]
+        public IWebElement buttonLogin { get; set; }
+    }
+}
---
+++ TestApplication/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using TestApplication.Pages;
+
+namespace TestApplication
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver _driver;
+        private const string URL = "localhost:3333";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _driver = new FirefoxDriver();
+            _driver.Navigate().GoToUrl(URL);
+
+            Login();
+        }
+
+        public void Login()
+        {
+            StartPage page = new StartPage(_driver);
+            page.linkLogin.Click();
+        }
+    }
+}
